4ca0df6c1af511a3d06f38272198f51535763b23
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-11-17 10:04:43 +0100

    adding tailwindcss and implementing html color block in main page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome Yohann"/>
+  <div class="flex flex-col min-h-screen">
+    <!-- Navbar Menu -->
+    <div class="p-6 bg-blue-500 text-white">
+      <p>Navbar Menu</p>
+      <!-- Placez ici le contenu de votre Navbar -->
+    </div>
+
+    <!-- En-tête (Hero) -->
+    <div class="p-6 bg-green-500 text-white flex-grow">
+      <p>En-tête (Hero)</p>
+      <!-- Contenu de votre En-tête -->
+    </div>
+
+    <!-- Liste de produits (ProductList) -->
+    <div class="p-6 bg-red-500 text-white flex-grow">
+      <p>Liste de produits</p>
+      <!-- Contenu de votre liste de produits -->
+    </div>
+
+    <!-- Pied de page (Footer) -->
+    <div class="p-6 bg-yellow-500 text-white mt-auto">
+      <p>Pied de page</p>
+      <!-- Placez ici le contenu de votre Footer -->
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
