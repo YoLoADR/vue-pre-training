a1313feb6619850b750d689f387bcc00ad4259cf
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-11-21 10:51:19 +0100

    auth guard

--- src/db/mainDB.js
+++ src/db/mainDB.js
@@ -3,7 +3,7 @@ export const mainDB = {
     headerNavLinks: [
       { name: 'Accueil', href: '/', current: true },
       { name: 'À propos', href: '/about', current: false },
-      { name: 'Utilisateurs', href: '/user', current: false },
+      { name: 'UserProfile', href: '/user-profile', current: false },
       { name: 'Produits', href: '/produits', current: false },
       { name: 'Contact', href: '/contact', current: false },
     ]
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '@/views/HomePage.vue';
 // import About from '../views/About.vue';
-// import Users from '../views/Users.vue';
+import UserProfile from '../views/UserProfile.vue';
 // import Products from '../views/Products.vue';
 import ProductDetail from '@/views/ProductDetail.vue';
 import ContactPage from '@/views/ContactPage.vue';
@@ -12,7 +12,19 @@ import ContactPage from '@/views/ContactPage.vue';
 const routes = [
   { path: '/', name: 'Accueil', component: HomePage },
 //   { path: '/about', name: 'À propos', component: About },
-//   { path: '/user', name: 'Utilisateurs', component: Users },
+  {
+    path: '/user-profile',
+    name: 'UserProfile',
+    component: UserProfile,
+    beforeEnter: (to, from, next) => {
+      // Supposons que 'isUserAuthenticated' est une méthode pour vérifier si l'utilisateur est connecté
+      if (isUserAuthenticated()) {
+        next(); // Autorise l'accès à la route
+      } else {
+        next('/'); // Redirige vers la page d'accueil ou une page de connexion
+      }
+    }
+  },
 //   { path: '/produits', name: 'Produits', component: Products },
  {  path: '/produits/:id', // Paramètre dynamique ':id' 
     name: 'ProductDetail',
@@ -24,5 +36,12 @@ const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
   });
+
+  function isUserAuthenticated() {
+    // Ici, vous devriez implémenter la logique de vérification d'authentification
+    // Par exemple, vérifier un token dans localStorage ou un état de store Vuex
+    return false; // ou false selon l'état d'authentification
+  }
+  
 
 export default router;
