50853acaf12e77030c71a19a837c129cfab58909
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-11-21 07:13:25 +0100

    adding form

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <HomePage />
+  <ContactPage />
 </template>
 
 <script>
 import HomePage from '@/views/HomePage';
+import ContactPage from '@/views/ContactPage';
 
 export default {
   name: 'App',
   components: {
     HomePage,
+    ContactPage
   }
 }
 </script>
