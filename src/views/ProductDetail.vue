ce4902c9ddc4e82682154e4e704c3c9c9adeca87
yohann ravino <atticot.yohann@gmail.com>
2024-10-16 07:23:45 +0200

    route params id

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -5,17 +5,20 @@
         <!-- Afficher plus de détails sur le produit ici -->
     </div>
 </template>
-  
-<script>
-export default {
-    name: 'ProductDetail',
-    data() {
-        return {
-            productId: null
-        };
-    },
-    created() {
-        this.productId = this.$route.params.id; // Accéder au paramètre 'id' de la route
-    }
-}
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+// Création d'une référence réactive pour stocker l'ID du produit
+const productId = ref(null);
+
+// Obtention de l'objet route
+const route = useRoute();
+
+// Utilisation de onMounted pour accéder aux paramètres de route après le montage du composant
+onMounted(() => {
+    // Accéder au paramètre 'id' de la route
+    productId.value = route.params.id;
+});
 </script>
